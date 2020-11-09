ba172f1410ae4a1facaec55403d8e40952ab8ea8
Guilherme Gomes <gtechmedia@gmail.com>
2020-11-08 22:31:13 -0300

    Solution almost done

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cryptography, Anagrams and String Hashing exercises
+
+### Running Instructions
+
+**(\*) These code examples needs Node.JS installed**
+
+Just uncomment the method's call you want to run and type on your terminal:
+```js
+  node index.js
+```
--- index.js
+++ index.js
@@ -25,21 +25,28 @@ function EncryptString(rawString) {
       : character;
   });
 
-  return console.log(encryptedString);
+  console.log(encryptedString);
 }
 
 /**
- * Notice to revisors: Javascript doens't provides native support to multidiomensional arrays
+ * NOTICE: Javascript doens't provides native support to multidiomensional arrays
  */
 function ValidateMagicSquare(arr1, arr2, arr3) {
   const messages = {
     alert: 'A matriz informada não é um Quadrado Mágico :/',
     sucess: 'Parabéns! A matriz informada é um Quadrado Mágico!',
     error:
-      'Esta função espera 3 parâmetros do tipo array contendo 3 caracteres numéricos em cada um',
+      'Ops, informe como parâmetros de entrada 3 listas contendo 3 dígitos numéricos em cada',
   };
 
-  if (!arr1 || !arr2 || !arr3) {
+  if (
+    !arr1 ||
+    !arr2 ||
+    !arr3 ||
+    arr1.length < 3 ||
+    arr2.length < 3 ||
+    arr3.length < 3
+  ) {
     return console.error(messages.error);
   }
 
@@ -49,77 +56,79 @@ function ValidateMagicSquare(arr1, arr2, arr3) {
   const diag1 = () => matrix[0][0] + matrix[1][1] + matrix[2][2];
   const diag2 = () => matrix[2][0] + matrix[1][1] + matrix[0][2];
 
-  // const rows = [];
   for (let i = 0; i < matrix.length; i++) {
     const row = matrix[i].reduce(reducer);
     if (row !== 15) return console.error(messages.alert);
-    // rows.push({ [`row${i}`]: row });
   }
 
-  // const columns = [];
   for (let i = 0; i < matrix.length; i++) {
     const column = matrix[i].reduce(reducer);
     if (column !== 15) return console.error(messages.alert);
-    // columns.push({ [`col${i}`]: column });
   }
 
-  // const diagonals = [];
   for (let i = 0; i < matrix.length - 1; i++) {
     const diagonal =
       i === 0 ? matrix[i].reduce(diag1) : matrix[i].reduce(diag2);
     if (diagonal !== 15) return console.error(messages.alert);
-    // diagonals.push({ [`diag${i}`]: diagonal });
   }
 
-  return console.log(messages.sucess);
+  console.log(messages.sucess);
 }
 
-function FindEncryptedWord(string, hash = null) {
+/**
+ * NOTICE: Implementation NOT completed
+ */
+function FindEncryptedWord() {
   const HASH = 7;
   const FACTOR = 37;
   const LETTERS = 'acdegilmnoprstuw';
+  const LOOKED_HASH = 695527946727;
 
-  const getHash = (string) => {
-    let h = HASH;
-    const Ilen = string.length;
+  let hasFoundHash = false;
+  let currentAnagram = '';
+  let generatedAnagrams = [];
 
-    for (let i = 0; i < Ilen; i++) {
-      h = h * FACTOR + LETTERS.indexOf(string.charAt(i));
-      return h;
+  const generateAnagram = (length) => {
+    var anagram = '';
+    for (var i = 0; i < length; i++) {
+      anagram += LETTERS.charAt(Math.floor(Math.random() * LETTERS.length));
     }
+
+    if (generatedAnagrams.includes(anagram)) {
+      generateAnagram(HASH);
+    }
+
+    return anagram;
   };
 
-  const reverseHash = (hash) => {
-    let h = HASH;
-    const Ilen = hash.length;
+  const getHash = (payload) => {
+    console.log(payload);
 
-    for (let i = 0; i < Ilen; i++) {
-      h = h / FACTOR + LETTERS.indexOf(hash(i));
-      return h;
+    let h = HASH;
+    let payloadLength = payload.length;
+
+    for (i = 0; i < payloadLength; i++) {
+      h = h * FACTOR + LETTERS.indexOf(payload.charAt(i));
     }
+
+    return h;
   };
 
-  console.log(getHash(string));
-  console.log(reverseHash(hash));
-}
+  do {
+    currentAnagram = generateAnagram(HASH);
+    let hash = getHash(currentAnagram);
 
-function teste() {
-  const hash = 7;
-  const factor = 37;
-  const letters = 'acdegilmnoprstuw';
-  const stringHash = '695527946727';
-  const mod = 16;
+    if (hash === LOOKED_HASH) {
+      hasFoundHash = true;
+      break;
+    }
 
-  const palavraPura = 'amor';
-  const indicesLetras = '0-7-9-11';
-  const hashLetraA = '7 * 37 + 0 = 259';
-  const hashLetraM = '7 * 37 + 7 = 266';
-  const hashLetraO = '7 * 37 + 9 = 268';
-  const hashLetraR = '7 * 37 + 11 = 270';
-  const hashFinal = '259266268270';
+    generatedAnagrams.push(currentAnagram);
+  } while (!hasFoundHash);
+
+  console.log('WORD_FOUND', currentAnagram);
 }
 
 // EncryptString('Luiza');
 // ValidateMagicSquare([4, 9, 2], [3, 5, 7], [8, 1, 6]);
-// FindEncryptedWord('teste', 695527946727);
-teste();
+// FindEncryptedWord();
